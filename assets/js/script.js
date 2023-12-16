7bce5079bfe02be0555efd1770a14dae020ab97c
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-16 22:00:20 +0530

    toggle added

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,30 @@ window.addEventListener("load", function () {
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 });
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+/**
+ * NAVBAR
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function (){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
