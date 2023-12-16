036ae6182934cc6ca64b903d01a1a6fa2be42ffd
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-16 22:12:47 +0530

    header done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 
-
 /**
  * PRELOAD
  *
@@ -34,10 +33,37 @@ const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
 
-const toggleNavbar = function (){
+const toggleNavbar = function () {
     navbar.classList.toggle("active");
     overlay.classList.toggle("active");
     document.body.classList.toggle("nav-active");
 }
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
+
+/**
+ * HEADER
+ */
+
+const header = document.querySelector("[data-header]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+    const isScrollBottom = lastScrollPos < window.scrollY;
+    if (isScrollBottom) {
+        header.classList.add("hide");
+    } else {
+        header.classList.remove("hide");
+    }
+    lastScrollPos = window.scrollY;
+}
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 50) {
+        header.classList.add("active");
+        hideHeader();
+    } else {
+        header.classList.remove("active");
+    }
+})
